82784b7b1f64cb8bf78f90ce2dcf6848f3485f1d
jawadrada <jawadrada18@gmail.com>
2024-06-23 00:48:55 -0400

    Added Routing to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
-import Globe_image from "./assets/Globe_image.webp"
+import { useRoutes } from 'react-router-dom';
+import Home from "./pages/Home"
+import AddCreator from "./pages/AddCreator"
+import EditCreator from "./pages/EditCreator"
+import ShowCreators from "./pages/ShowCreators"
+import ViewCreator from "./pages/ViewCreator"
 
 function App() {
-  return (
-    <>
-      <div className="relative h-screen">
-        <img className="w-full h-full bject-cover" src={Globe_image} alt="Background Image of a globe" />
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/creators", element: <ShowCreators /> },
+    { path: "/creator:id", element: <ViewCreator /> },
+    { path: "/edit:id", element: <EditCreator /> },
+    { path: "/add", element: <AddCreator /> },
+  ])
 
-        <div className="absolute inset-0 flex flex-col items-center justify-center md:-top-20 lg:-top-28 z-10">
-          <h1 className="font-mono gradient-text text-6xl md:text-7xl lg:text-9xl font-extrabold mb-16">Creator Verse</h1>
-
-          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-12">
-            <button className="w-72 h-12 lg:h-16 bg-indigo-500 text-center text-white text-lg md:text-xl font-semibold cursor-pointer px-8 py-4 rounded transition hover:bg-indigo-600 hover:border hover:border-white">
-              VIEW ALL CREATORS
-            </button>
-
-            <button className="w-72 h-12 lg:h-16 bg-indigo-500 text-center text-white text-lg md:text-xl font-semibold cursor-pointer px-8 py-4 rounded transition hover:bg-indigo-600 hover:border hover:border-white">
-              ADD A CREATOR
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  return routes
 }
 
 export default App
