4c1582ffae172de34c67cf27feb82a36554a6032
jawadrada <jawadrada18@gmail.com>
2024-06-27 00:07:24 -0400

    Updated Home
    Adjusted the size of the height of the Homepage to more accuratly reflect the desired design from the prototype provided

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Globe_image from "../assets/Globe_image.webp"
 function Home() {
   return (
     <>
-        <div className="relative h-screen">
+        <div className="relative h-[75vh] border-b-2 border-white">
             <img className="w-full h-full object-cover" src={Globe_image} alt="Background Image of a globe" />
 
             <div className="absolute inset-0 flex flex-col items-center justify-center md:-top-20 lg:-top-28 z-10">
@@ -14,13 +14,13 @@ function Home() {
                 <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-12">
                     <Link to="/creators">
                         <button className="w-72 h-12 lg:h-16 bg-indigo-500 text-center text-white text-lg md:text-xl font-semibold cursor-pointer px-8 py-4 rounded transition hover:bg-indigo-600 hover:border hover:border-white">
-                        VIEW ALL CREATORS
+                            VIEW ALL CREATORS
                         </button>
                     </Link>
 
                     <Link to="/add"> 
                         <button className="w-72 h-12 lg:h-16 bg-indigo-500 text-center text-white text-lg md:text-xl font-semibold cursor-pointer px-8 py-4 rounded transition hover:bg-indigo-600 hover:border hover:border-white">
-                        ADD A CREATOR
+                            ADD A CREATOR
                         </button>
                     </Link>
                 </div>
